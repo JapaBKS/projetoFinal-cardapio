0906efc6cc9a9248eaa997ef830215359f39d4a8
Bruno Sakaguchi <bruno.kioshi@pucpr.edu.br>
2024-06-13 19:35:41 -0300

    lista dos itens do cardapio

---
+++ cardapio.py
@@ -0,0 +1,72 @@
+cardapio = {
+    'Bebidas': {
+        'Refrigerantes': [
+            {'Nome': 'Coca-Cola', 'Preço': 6},
+            {'Nome': 'Pepsi', 'Preço': 6},
+            {'Nome': 'Guaraná Jesus', 'Preço': 6},
+            {'Nome': 'Fanta Uva', 'Preço': 6}
+        ],
+        'Alcoólicos': [
+            {'Nome': 'Vinho', 'Preço': 60},
+            {'Nome': 'Whiskey', 'Preço': 35},
+            {'Nome': 'Gin Tônica', 'Preço': 35},
+            {'Nome': 'Drinks', 'Preço': 30}
+        ],
+        'Sucos': [
+            {'Nome': 'Abacaxi', 'Preço': 15},
+            {'Nome': 'Maracujá', 'Preço': 15},
+            {'Nome': 'Limão', 'Preço': 15},
+            {'Nome': 'Laranja', 'Preço': 15},
+            {'Nome': 'Manga', 'Preço': 15}
+        ],
+        'Quentes': [
+            {'Nome': 'Expresso', 'Preço': 5},
+            {'Nome': 'Capuccino', 'Preço': 10},
+            {'Nome': 'Mocaccino', 'Preço': 12},
+            {'Nome': 'Café Com Leite', 'Preço': 7},
+            {'Nome': 'Achocolatado', 'Preço': 10},
+            {'Nome': 'Chá', 'Preço': 7}
+        ]
+    },
+    'Entradas': {
+        'Vegetarianas': [
+            {'Nome': 'Salada', 'Preço': 30},
+            {'Nome': 'Batatas Fritas', 'Preço': 30},
+            {'Nome': 'Polenta', 'Preço': 25}
+        ],
+        'Com Derivados De Animais': [
+            {'Nome': 'Carpaccio', 'Preço': 50},
+            {'Nome': 'Creme De Aipim Com Bacon', 'Preço': 35},
+            {'Nome': 'Iscas De Peixe', 'Preço': 40},
+            {'Nome': 'Carne De Onça', 'Preço': 35}
+        ]
+    },
+    'Pratos Principais': {
+        'Vegetarianas': [
+            {'Nome': 'Macarrão Alho E Óleo', 'Preço': 45},
+            {'Nome': 'Ratatouille', 'Preço': 60},
+            {'Nome': 'Macarrão Ao Molho 4 Queijos', 'Preço': 45},
+            {'Nome': 'Lasanha Com Carne De Soja', 'Preço': 50}
+        ],
+        'Com Derivados De Animais': [
+            {'Nome': 'Carré De Cordeiro', 'Preço': 85},
+            {'Nome': 'Bife À Parmegiana', 'Preço': 65},
+            {'Nome': 'Estrogonofe', 'Preço': 45},
+            {'Nome': 'Costelinha Barbecue', 'Preço': 70}
+        ]
+    },
+    'Sobremesas': {
+        'Com Lactose': [
+            {'Nome': 'Petit Gâteau', 'Preço': 28},
+            {'Nome': 'Pudim', 'Preço': 22},
+            {'Nome': 'Banana Split', 'Preço': 25},
+            {'Nome': 'Torta De Limão', 'Preço': 15}
+        ],
+        'Sem Lactose': [
+            {'Nome': 'Gelatina', 'Preço': 10},
+            {'Nome': 'Salada De Frutas', 'Preço': 25},
+            {'Nome': 'Crumble De Maçã', 'Preço': 35},
+            {'Nome': 'Brigadeiro Vegano', 'Preço': 15}
+        ]
+    }
+}
